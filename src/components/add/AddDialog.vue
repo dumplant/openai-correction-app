2e70ba8ab35095e55de8c37fa3ce5cbf14232a42
dumplant <2437789306@qq.com>
2023-07-12 23:28:45 +0800

    修改响应部分基本完成

--- src/components/add/AddDialog.vue
+++ src/components/add/AddDialog.vue
@@ -2,25 +2,43 @@
   <div>
     <el-button @click="dialogVisible = true">新建</el-button>
 
-    <el-dialog title="请开始写作" :visible.sync="dialogVisible" width="50%" height="80%" :before-close="handleClose">
-      <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="作文类型">
-          <el-cascader v-model="form.value" :options="options"></el-cascader>
-        </el-form-item>
-        <el-form-item label="作文标题">
-          <el-input v-model="form.title" type="textarea" :rows="3" placeholder="请输入"></el-input>
+    <el-dialog title="请开始写作" :visible.sync="dialogVisible" width="70%" height="100%" :before-close="handleClose">
+      <div style="height: 600px;overflow: auto">
+        <el-col :span="12">
+          <el-row>
+            <el-form ref="form" :model="form" label-width="80px" style="margin-right: 20px;">
+              <el-form-item label="作文类型">
+                <el-cascader v-model="form.value" :options="options"></el-cascader>
+              </el-form-item>
+              <el-form-item label="作文标题">
+                <el-input v-model="form.title" type="textarea" :rows="3" placeholder="请输入"></el-input>
+              </el-form-item>
+              <el-form-item label="作文正文">
+                <el-input type="textarea" :rows="15" placeholder="请输入" v-model="form.textarea"></el-input>
+              </el-form-item>
 
-        </el-form-item>
-        <el-form-item label="作文正文">
-          <el-input type="textarea" :rows="10" placeholder="请输入" v-model="form.textarea"></el-input>
-        </el-form-item>
+            </el-form>
+          </el-row>
+          <el-row>
+            <div style="float: right; margin-right: 20px;">
+              <span style="margin-right: 1rem;">字数统计：{{ wordCount }}</span>
+              <el-button @click="dialogVisible = false">保 存</el-button>
+              <el-button type="primary" @click="toGrade()">批 改</el-button>
+            </div>
 
-      </el-form>
-      <span slot="footer" class="dialog-footer">
-        <span style="margin-right: 1rem;">字数统计：{{ wordCount }}</span>
-        <el-button @click="dialogVisible = false">保 存</el-button>
-        <el-button type="primary" @click="toGrade()">批 改</el-button>
-      </span>
+          </el-row>
+        </el-col>
+        <el-col :span="12">
+          <ResponseArea :gradeRes="gradeRes" />
+        </el-col>
+      </div>
+      <!-- <span slot="footer" class="dialog-footer">
+        <el-col :span="12">
+          <span style="margin-right: 1rem;">字数统计：{{ wordCount }}</span>
+          <el-button @click="dialogVisible = false">保 存</el-button>
+          <el-button type="primary" @click="toGrade()">批 改</el-button>
+        </el-col>
+      </span> -->
     </el-dialog>
   </div>
 </template>
@@ -28,8 +46,11 @@
 <script>
 /* eslint-disable */
 import getResponse from '@/utils/GetResponse'
-
+import ResponseArea from '@/components/response/ResponseArea.vue'
 export default {
+  components: {
+    ResponseArea
+  },
   data() {
     return {
       form: {
@@ -65,7 +86,7 @@ export default {
         value: '雅思',
         label: '雅思',
       },],
-      gradeRes: ''
+      gradeRes: {}
     };
   },
   computed: {
@@ -80,6 +101,9 @@ export default {
     async toGrade() {
       // this.dialogVisible = false;
       this.gradeRes = await getResponse(this.form.value, this.form.textarea);
+      const jsonRegex = /\{(?:[^{}]|(?:\{(?:[^{}]|[^{}]*\}))*[^{}]*)*\}/g;
+      const jsonMatches = this.gradeRes.match(jsonRegex);
+      this.gradeRes = JSON.parse(jsonMatches)
       console.log(this.gradeRes);
     },
     handleClose(done) {
---
+++ src/components/response/ResponseArea.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+      <el-tab-pane label="修改" name="first">
+        <!-- <el-col :span="12"> -->
+        <div class="grid-content bg-purple-light">
+          <div class="outputBox" v-html="res.polishedVersion" v-loading="loading" contenteditable="false" spellcheck="false">
+          </div>
+          <div class="buttonGroup">
+            <el-button type="primary" title="复制" circle icon="el-icon-document-copy" size="small" @click="onCopy"></el-button>
+            <el-button type="warning" title="收藏" circle icon="el-icon-star-off" size="small" @click="collect"></el-button>
+            <el-button type="success" title="显示差异" circle icon="el-icon-view" size="small" @click="switchContent"></el-button>
+          </div>
+        </div>
+        <!-- </el-col> -->
+      </el-tab-pane>
+      <el-tab-pane label="得分" name="second">得分
+
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['gradeRes'],
+  data() {
+    return {
+
+      activeName: 'first'
+    };
+  },
+  computed: {
+    res: function () {
+      return this.gradeRes // 直接监听props里的status状态
+    }
+  },
+
+  methods: {
+    handleClick(tab, event) {
+      console.log("this.res = " + this.res);
+      console.log(tab, event);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.outputBox {
+  font-family: monospace;
+  background: #feffff;
+  overflow: auto;
+  height: 19rem;
+  font-size: 1.32rem;
+  display: block;
+  padding: 5px 15px;
+  line-height: 1.5;
+  box-sizing: border-box;
+  width: 100%;
+  color: #606266;
+  background-color: #FFF;
+  background-image: none;
+  border: 1px solid #DCDFE6;
+  border-radius: 4px;
+  transition: border-color .2s cubic-bezier(.645, .045, .355, 1);
+}
+
+.buttonGroup {
+  width: 100%;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+</style>
